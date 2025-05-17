e2b480b450ff55dd98a69210678eeff5ce3d63b0
mehediemon1437 <mdmehedihasanemon1437@gmail.com>
2025-05-17 16:26:52 +0600

    serverside deployed

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://bistro-boss-22996.web.app",
+    "https://bistro-boss-38069.web.app",
   ],
   credentials: true,
   optionSuccessStatus: 200,
@@ -65,24 +66,22 @@ async function run() {
         req.decoded = decoded;
         next();
       });
-
-      // next()
     };
 
     // Use verify admin after verify token
-    const  verifyAdmin =async (req,res,next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email:email}
-      const  user = await usersCollection.findOne(query)
-      const isAdmin = user?.role === 'admin'
-      if(!isAdmin){
-        return res.status(403).send({message:'Forbidden Access'})
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "Forbidden Access" });
       }
-      next()
-    }
+      next();
+    };
 
     // ! users Related API
-    app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -110,20 +109,25 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-    app.patch("/users/admin/:id",verifyToken,verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const filter = {
-        _id: new ObjectId(id),
-      };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await usersCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
-    app.delete("/users/:id",verifyToken,verifyAdmin, async (req, res) => {
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = {
+          _id: new ObjectId(id),
+        };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = {
         _id: new ObjectId(id),
@@ -132,9 +136,14 @@ async function run() {
       res.send(result);
     });
 
-    // Menu related Apis
+    //! Menu related Apis
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
       res.send(result);
     });
 
@@ -162,6 +171,21 @@ async function run() {
       res.send(result);
     });
 
+    // ! PAyment Intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, "amount inside the intent");
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -169,7 +193,6 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
